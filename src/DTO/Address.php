001a4fdd8900113fd108015a130128890147c4d7
qschmick <qschmick@users.noreply.github.com>
2022-01-19 21:38:33 +0000

    Fix styling

--- src/DTO/Address.php
+++ src/DTO/Address.php
@@ -2,7 +2,6 @@
 
 namespace BluefynInternational\ShipEngine\DTO;
 
-use BluefynInternational\ShipEngine\DTO\Validators\GreaterThanOrEquals;
 use BluefynInternational\ShipEngine\DTO\Validators\InArray;
 use BluefynInternational\ShipEngine\DTO\Validators\MaxLength;
 use BluefynInternational\ShipEngine\DTO\Validators\MinLength;
--- src/DTO/ParsedAddress.php
+++ src/DTO/ParsedAddress.php
@@ -2,10 +2,6 @@
 
 namespace BluefynInternational\ShipEngine\DTO;
 
-use BluefynInternational\ShipEngine\DTO\Validators\GreaterThanOrEquals;
-use BluefynInternational\ShipEngine\DTO\Validators\InArray;
-use BluefynInternational\ShipEngine\DTO\Validators\MaxLength;
-use BluefynInternational\ShipEngine\DTO\Validators\MinLength;
 use Spatie\DataTransferObject\DataTransferObject;
 
 class ParsedAddress extends DataTransferObject
