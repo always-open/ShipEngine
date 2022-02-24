047a2e324ba451d963a5a69af5cb65e424d9b48d
quentin.schmick <quentin.schmick@nathanjames.com>
2022-02-24 10:07:49 -0500

    Removed redundent code

--- src/Util/ShipEngineLogger.php
+++ src/Util/ShipEngineLogger.php
@@ -21,35 +21,14 @@ final class ShipEngineLogger implements LoggerInterface
 
         switch ($level) {
             case LogLevel::EMERGENCY:
-                $this->emergency($message, $context);
-
-                break;
             case LogLevel::ALERT:
-                $this->alert($message, $context);
-
-                break;
             case LogLevel::CRITICAL:
-                $this->critical($message, $context);
-
-                break;
             case LogLevel::ERROR:
-                $this->error($message, $context);
-
-                break;
             case LogLevel::WARNING:
-                $this->warning($message, $context);
-
-                break;
             case LogLevel::NOTICE:
-                $this->notice($message, $context);
-
-                break;
             case LogLevel::INFO:
-                $this->info($message, $context);
-
-                break;
             case LogLevel::DEBUG:
-                $this->debug($message, $context);
+                $this->{$level}($message, $context);
 
                 break;
             default:
