0d591fbaea34f0e14c23f091b9d1abd880ff2a21
Lucas Graciano <lucas.nexus@gmail.com>
2022-03-09 12:00:21 -0800

    ShipmentCustomsItem: Revert type update, Update constructor to handle value float->valid CurrencyAmount conversion

--- src/DTO/ShipmentCustomsItem.php
+++ src/DTO/ShipmentCustomsItem.php
@@ -13,10 +13,27 @@ class ShipmentCustomsItem extends DataTransferObject
     public string|null $description;
     #[GreaterThanOrEquals(0)]
     public int $quantity;
-    public float $value;
+    public CurrencyAmount $value;
     public string|null $harmonized_tariff_code;
     public string|null $country_of_origin;
     public string|null $unit_of_measure;
     public string|null $sku;
     public string|null $sku_description;
+
+    public function __construct(...$args)
+    {
+        $temp = $args;
+        if (is_array($temp[0] ?? null)) {
+            $temp = $temp[0];
+        }
+
+        if(is_float($temp['value'])) {
+            $args['value'] = [
+                'currency' => 'usd',
+                'amount'  => $temp['value'],
+            ];
+        }
+
+        parent::__construct($args);
+    }
 }
