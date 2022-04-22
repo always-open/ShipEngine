09109951bd1f3703c9f90550df64671dca922c52
Lucas Graciano <lucas.nexus@gmail.com>
2022-04-22 10:42:07 -0700

    ShipEngineClient: Handle all exceptions when making requests

--- src/ShipEngineClient.php
+++ src/ShipEngineClient.php
@@ -5,11 +5,13 @@ namespace BluefynInternational\ShipEngine;
 use BluefynInternational\ShipEngine\Message\RateLimitExceededException;
 use BluefynInternational\ShipEngine\Message\ShipEngineException;
 use BluefynInternational\ShipEngine\Models\RequestLog;
+use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Http\Response;
+use Throwable;
 
 class ShipEngineClient
 {
@@ -184,7 +186,7 @@ class ShipEngineClient
                 $request,
                 ['timeout' => $config->timeout->s, 'http_errors' => false]
             );
-        } catch (ClientException $err) {
+        } catch (Exception|Throwable  $err) {
             if (config('shipengine.track_requests')) {
                 $requestLog->exception = substr($err->getMessage(), 0, 255);
                 $requestLog->save();
