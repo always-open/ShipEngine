bd838f4d43358e483a37cb16b026d885dff5d424
Quentin Schmick <quentin.schmick@nathanjames.com>
2022-08-01 15:44:08 -0400

    Updated logger version and removed unneeded observer (#17)

--- src/Models/RequestLog.php
+++ src/Models/RequestLog.php
@@ -3,7 +3,6 @@
 namespace BluefynInternational\ShipEngine\Models;
 
 use AlwaysOpen\RequestLogger\Models\RequestLogBaseModel;
-use BluefynInternational\ShipEngine\Observers\RequestLogObserver;
 
 /**
  * BluefynInternational\ShipEngine\Models\RequestLog
@@ -19,12 +18,6 @@ use BluefynInternational\ShipEngine\Observers\RequestLogObserver;
  */
 class RequestLog extends RequestLogBaseModel
 {
-    protected static function boot()
-    {
-        parent::boot();
-        parent::observe(RequestLogObserver::class);
-    }
-
     public function getTable()
     {
         return config('shipengine.request_log_table_name');
